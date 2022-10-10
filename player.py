a225cffe85e1d9b328636b47c1744f7ae8e97593
Kason <kason.suchow@gmail.com>
2022-10-09 20:29:06 -0600

    Finishing #2 and player movment
    Decided on profile based movement system. Roughly implemented. Shitty art...

--- player.py
+++ player.py
@@ -8,6 +8,8 @@ class Player():
         self.x = 500
         self.y = 400
 
+        self.pos = (self.x, self.y)
+
         # Velocity variables
         self.x_velocity = 0
         self.y_velocity = 0
@@ -15,20 +17,36 @@ class Player():
 
         # Bounding box variables
         self.width = 25
-        self.height = 25
+        self.height = 40
+
+        # Player sprites
+        self.right_sprite = pygame.image.load("assets/player/right.png")
+        self.left_sprite = pygame.image.load("assets/player/left.png")
+        self.away_sprite = pygame.image.load("assets/player/away.png")
+        self.forward_sprite = pygame.image.load("assets/player/forward.png")
 
         # Generic player variables
         self.color = (255, 255, 255)
+        self.facing = "right"
 
     # Handles the drawing of all items player related (requires a drawable pygame.surface element, we use the game.screen var)
     def draw(self, surface):
-        pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height), 0) # Placeholder white square to indicate the player
+        if self.facing == "right":
+            surface.blit(self.right_sprite, self.pos)
+        elif self.facing == "left":
+            surface.blit(self.left_sprite, self.pos)
+        elif self.facing == "away":
+            surface.blit(self.away_sprite, self.pos)
+        elif self.facing == "forward":
+            surface.blit(self.forward_sprite, self.pos)
 
     # Hanldes all non-graphical updates and events (requires access to the game.events var)
     def update(self, events):
         # Update player location based on player velocity
         self.x += self.x_velocity
         self.y += self.y_velocity
+
+        self.pos = (self.x, self.y)
 
         self.movementController(events)
 
@@ -39,14 +57,18 @@ class Player():
                 # Check for left/right movement
                 if event.key == pygame.K_a:
                     self.x_velocity = -self.speed
+                    self.facing = "left"
                 elif event.key == pygame.K_d:
                     self.x_velocity = self.speed
+                    self.facing = "right"
 
                 # Check for up/down movement
                 if event.key == pygame.K_w:
                     self.y_velocity = -self.speed
+                    self.facing = "away"
                 elif event.key == pygame.K_s:
                     self.y_velocity = self.speed
+                    self.facing = "forward"
 
             if event.type == pygame.KEYUP: # Check for the keyup event to remove velocity
                 # Kill horizontal velocity

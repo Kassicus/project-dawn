6e46e4370a837e2914ab7938ddb75ca2b4c46fb2
metaphysicalcrab <tristonritter9@gmail.com>
2022-10-10 16:11:08 -0600

    Compressed weapon classes into one class

--- item.py
+++ item.py
@@ -7,29 +7,50 @@ class Item():
     def __init__(self, itemName, itemDescription):
         self.itemName = itemName
         self.itemDescription = itemDescription
-        self.requiresManySoulTypes = False
+        self.requiresManySoulTypes = False         # If false, only one price must be paid. If true, all prices must be paid. EX: FALSE = Item costs 1 mini boss OR 50 mobs. TRUE = Item costs 1 mini boss AND 50 mobs.
         self.mobSoulPrice = 0
         self.miniBossSoulPrice = 0
         self.regionBossSoulPrice = 0
         
-class OneHandedWeapon(Item):
-    def __init__(self, damage, atkSpeed):
-        self.isTwoHanded = False
+# Weapon Subclass
+# Has all attributes of Item class
+## Can be be part of the same loadout as another 
+## weapon as long is they're both one handed. Maybe 
+## hot swap between them on a keybind.
+class Weapon(Item):
+    def __init__(self, damage, atkSpeed, isRanged, isTwoHanded):
+        self.isTwoHanded = isTwoHanded
+        self.isRanged = isRanged
         self.damage = damage
         self.atkSpeed = atkSpeed
 
-class TwoHandedWeapon(Item):
-    def __init__(self):
-        self.isTwoHanded = True
-        self.damage = 2
-        self.atkSpeed = 0
+# Two Handed Weapon Subclass
+# Has all attributes of Item class
+## Takes up both hands. So can't weapon swap.
+## More powerful than a one handed weapon.
+## Maybe give them an active ability?
+###class TwoHandedWeapon(Item):
+###    def __init__(self):
+###        self.isTwoHanded = True
+###        self.isRanged
+###        self.damage = 2
+###        self.atkSpeed = 0
 
+# Offensive Magic Subclass
+# Has all attributes of Item class
+## Takes up its own slot on the hotbar.Thus
+## can be used alongside the players weapon choice.
+## Variety of effects (depending on how crazy we get)
 class DestructionMagic(Item):
     def __init__(self, damage, coolDownTimer, magicCharges):
         self.damage = damage
         self.coolDownTimer = coolDownTimer
         self.magicCharges = magicCharges
 
+# Healing Magic Subclass
+# Has all attributes of Item class
+## Purchasable with souls. Allows player to heal during
+## combat a variable amount of times.
 class RestorationMagic(Item):
     def __init__(self, healAmount, coolDownTimer, magicCharges):
         self.healAmount = healAmount
--- itemLib.py
+++ itemLib.py
@@ -7,13 +7,13 @@ import item
 #------------- Weapon Library ---------------#
 #                                            #
 ## Starter Pistol ##
-pistol1 = item.OneHandedWeapon(1,1)
+pistol1 = item.Weapon(1,1,True,False)
 pistol1.itemName = "Pistol"
 pistol1.itemDescription = "A pistol that you start the game with. Good luck."
 ####################
 
 ## Starter Secondary ##
-knife1 = item.OneHandedWeapon(1,2)
+knife1 = item.Weapon(1,2,False,False)
 knife1.itemName = "Butter Knife"
 knife1.itemDescription = "Your only line of defense when things get too close. Hope it isn't plastic."
 #######################
@@ -37,6 +37,14 @@ iceMagic1 = item.DestructionMagic(5,1,2)
 iceMagic1.itemName = "Icicle of Murderous Intent"
 iceMagic1.itemDescription = "Shoots a spike of ice that homes in on the nearest enemy and exploads on contact dealing its damage in an AOE."
 iceMagic1.miniBossSoulPrice = 1
+iceMagic1.mobSoulPrice = 50
+
+fireMagic1 = item.DestructionMagic(500,10,1)
+fireMagic1.itemName = "Soul Flare"
+fireMagic1.itemDescription = "A burning flare erupts from you dealing massive damage to whatever it hits"
+fireMagic1.regionBossSoulPrice = 2 
+fireMagic1.miniBossSoulPrice = 2 
+fireMagic1.requiresManySoulTypes = True
 ####################
 
 ## Restoration Magics ##

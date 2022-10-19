7a59738f4e9fb8a5cc10ec9f87b3f8f8d92657e4
metaphysicalcrab <tristonritter9@gmail.com>
2022-10-19 16:50:04 -0600

    #13 updated menu button section
    The menu text now all goes darker when the mouse is on the left side of the menu. still need to work on it to get it to be individual menu options and then also have something happen on click.

--- menu.py
+++ menu.py
@@ -27,10 +27,13 @@ class PlayerInventoryMenu():
         self.menuS1SS = {}         #menuSection1SubSections
         self.textCenterDict = {}   #Dictionary of Text Centered objects
         self.menuOptionDict = {}   #Dictionary of menuOptions
-
+        
         #Dictionary Dynamic Key Name Base Definitions
         self.menuS1SSKeyName = "menuSection1SubSection" #Base key name for a dynamic dictionary key. Dictionary:'menuSection1SubSections'. USE: Specifically to reference surface objects that are sub sections of menuSection1. EX: self.menuS1SSKeyname+str(1) = menuSection1SubSection1.
         self.textCenterS1SSC = "menuSection1SubSectionTextCenter" #Base key name for a dynamic dictionary key. Dictionary:'textCenterDict'. USE: Secifically to reference the objects that get a surface's dimensions and centers text within them. More Specifically for Section1's SubSections in this case. EX: self.textCenterS1SSC+str(1) = menuSection1SubSectionTextCenter1.
+        self.textCenterS2SSC = "menuSection2SubSectionTextCenter" #Base key name for a dynamic dictionary key. Dictionary:'textCenterDict'. USE: Secifically to reference the objects that get a surface's dimensions and centers text within them. More Specifically for Section2's SubSections in this case. EX: self.textCenterS1SSC+str(1) = menuSection1SubSectionTextCenter1.
+        self.textCenterS3SSC = "menuSection3SubSectionTextCenter" #Base key name for a dynamic dictionary key. Dictionary:'textCenterDict'. USE: Secifically to reference the objects that get a surface's dimensions and centers text within them. More Specifically for Section3's SubSections in this case. EX: self.textCenterS1SSC+str(1) = menuSection1SubSectionTextCenter1.
+        
         self.mO = "menuOption" #Base key name for a dynamic dictionary key. Dictionary:'menuOptionDict'. USE: Secifically to reference the text of the menu options. EX: self.mO+str(1) = menuOption1.
 
         self.display_surface = pygame.display.get_surface()        
@@ -45,6 +48,7 @@ class PlayerInventoryMenu():
 
         # Font Variables
         self.menuTitle = "Character Management"
+        self.menuOptionDict[self.mO] = "Filler Text"
         self.menuOptionDict[self.mO+str(1)] = "Character Info"
         self.menuOptionDict[self.mO+str(2)] = "Inventory"
         self.menuOptionDict[self.mO+str(3)] = "Astral Stash"
@@ -54,7 +58,7 @@ class PlayerInventoryMenu():
         self.menuTextSize = 28
 
         # Font Color Variables
-        self.textColor1 = "red3"
+        self.textColor1 = "red2"
 
         # Surface Color Variables
         self.menuSection2Color = self.colorPicker("gray22")
@@ -74,8 +78,8 @@ class PlayerInventoryMenu():
         self.menuColumnWidth = self.menuScreenBackground.get_width()/self.menuColumns #Variable for Column Width used in menu calculations
 
         self.menuScreenTitleSection = pygame.Surface((self.menuScreenBackground.get_width(),self.menuRowHeight))
-        self.menuMiddleRowSection = pygame.Surface((self.menuScreenBackground.get_width(),self.menuScreenBackground.get_height()))
-        self.menuBottomRowSection = pygame.Surface((self.menuScreenBackground.get_width(),self.menuScreenBackground.get_height()*2-self.menuRowHeight))
+        #self.menuMiddleRowSection = pygame.Surface((self.menuScreenBackground.get_width(),self.menuScreenBackground.get_height()))
+        #self.menuBottomRowSection = pygame.Surface((self.menuScreenBackground.get_width(),self.menuScreenBackground.get_height()*2-self.menuRowHeight))
 
         self.menuSection1 = pygame.Surface((self.menuColumnWidth,self.menuRowHeight*2),pygame.SRCALPHA)
         #self.menuSection1.fill(self.bg_color)
@@ -96,16 +100,33 @@ class PlayerInventoryMenu():
         self.menuSection3.fill(self.bg_color)
 
         #Text Centering Variables
+        ##Menu Title Section
+        self.menuTitleTextRect = self.font.get_rect(self.menuTitle, size = self.menuTitleTextSize)
+        self.menuTitleTextRect.center = self.menuScreenTitleSection.get_rect().center
+        ##Menu Section 1
         for s in range(1,self.menuRows+1):
             key = self.textCenterS1SSC+str(s)
             value = self.font.get_rect(self.menuOptionDict[self.mO+str(s)] , size = self.menuTextSize)
             value.center = self.menuS1SS[self.menuS1SSKeyName+str(s)].get_rect().center
             self.textCenterDict[key] = value
-
-        self.menuTitleTextRect = self.font.get_rect(self.menuTitle, size = self.menuTitleTextSize)
-        self.menuTitleTextRect.center = self.menuScreenTitleSection.get_rect().center
+        ##Menu Section 2
+        key = self.textCenterS2SSC
+        value = self.font.get_rect(self.menuOptionDict[self.mO], size = self.menuTextSize)
+        value.center = self.menuSection2.get_rect().center
+        self.textCenterDict[key] = value
+        ##Menu Section 3
+        key = self.textCenterS3SSC
+        value = self.font.get_rect(self.menuOptionDict[self.mO], size = self.menuTextSize)
+        value.center = self.menuSection3.get_rect().center
+        self.textCenterDict[key] = value
+        
 
     def draw(self):
+        mouse = pygame.mouse.get_pos()
+        if self.menuS1SS[self.menuS1SSKeyName+str(1)].get_width() <= mouse[0]:  #and self.menuS1SS[self.menuS1SSKeyName+str(1)].get_height() <= mouse[1]
+            color = self.textColor1
+        else: 
+            color = "red4"
         # Rendering the separate menu elements 'together'. Background/Containers/Text
         ##region -Renders menu title to the top row section of the menu
         self.font.render_to(
@@ -125,11 +146,35 @@ class PlayerInventoryMenu():
             self.menuS1SS[self.menuS1SSKeyName+str(i)],
             self.textCenterDict[self.textCenterS1SSC+str(i)],
             self.menuOptionDict[self.mO+str(i)],
-            self.textColor1,
+            color,
             size=self.menuTextSize,
             style=freetype.STYLE_UNDERLINE #| freetype.STYLE_OBLIQUE,
             )
             self.font.pad = False
+        ##endregion
+        ##region -Renders the sub sections of section 2
+        #for i in range(1,self.menuRows+1):
+        self.font.render_to(
+        self.menuSection2,
+        self.textCenterDict[self.textCenterS2SSC],
+        self.menuOptionDict[self.mO],
+        self.textColor1,
+        size=self.menuTextSize,
+        style=freetype.STYLE_UNDERLINE #| freetype.STYLE_OBLIQUE,
+        )
+        self.font.pad = False
+        ##endregion
+        ##region -Renders the sub sections of section 3
+        #for i in range(1,self.menuRows+1):
+        self.font.render_to(
+        self.menuSection3,
+        self.textCenterDict[self.textCenterS3SSC],
+        self.menuOptionDict[self.mO],
+        self.textColor1,
+        size=self.menuTextSize,
+        style=freetype.STYLE_UNDERLINE #| freetype.STYLE_OBLIQUE,
+        )
+        self.font.pad = False
         ##endregion
 
         self.display_surface.blit(self.menuScreenBackground,(self.x,self.y)) #MenuBackground
@@ -145,10 +190,10 @@ class PlayerInventoryMenu():
         #     pygame.draw.line(self.menuSection1,self.fg_color,(self.menuSection1ColumnWidth*i,self.menuSection1RowHeight),(self.menuSection1ColumnWidth*i,self.menuSection1.get_height()))
         #RegionEnd
         pygame.draw.rect(self.display_surface, self.menuSection2Color, (self.x, self.y+self.menuRowHeight, self.menuColumnWidth, self.menuRowHeight*2), 1) # MenuSection1Border
-        pygame.draw.rect(self.display_surface, self.menuSection2Color, (self.x+self.menuColumnWidth, self.y+self.menuRowHeight, self.menuColumnWidth*2, self.menuRowHeight*2), 1) # MenuSection1Border
-        pygame.draw.rect(self.display_surface, self.menuSection2Color, (self.x, self.y+self.menuRowHeight*3, self.menuColumnWidth*3, self.menuRowHeight*2), 1) # MenuSection1Border
+        pygame.draw.rect(self.display_surface, self.menuSection2Color, (self.x+self.menuColumnWidth, self.y+self.menuRowHeight, self.menuColumnWidth*2, self.menuRowHeight*2), 1) # MenuSection2Border
+        pygame.draw.rect(self.display_surface, self.menuSection2Color, (self.x, self.y+self.menuRowHeight*3, self.menuColumnWidth*3, self.menuRowHeight*2), 1) # MenuSection3Border
         pygame.draw.rect(self.display_surface, self.bg_color, (self.x, self.y, (self.width - (self.x * 2)), self.height - (self.y * 2)), 1) # MenuBorder
 
-    #def update(self, value):
-    #    self.value = value # Update the value with a live feed
-    #    self.current_value = int(self.width / self.max_value) * self.value # Refresh status bar width to match live value
+    def update(self, value):
+       self.value = value # Update the value with a live feed
+       self.current_value = int(self.width / self.max_value) * self.value # Refresh status bar width to match live value
